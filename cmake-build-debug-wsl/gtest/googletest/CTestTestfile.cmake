4e3b0bb8f6104050f7b678523185096533aab634
dxd <2080776216@qq.com>
2024-11-15 14:31:58 +0800

    init

--- cmake-build-debug-wsl/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/xd/catkin_ws/src/EngineeringAndCreationProject/cmake-build-debug-wsl/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
