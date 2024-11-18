c2a84ccaa69b773eab92b835ecf39504a5c2d38c
dxd <2080776216@qq.com>
2024-11-18 19:52:16 +0800

    fix unit error

--- src/test_vel_ctl.cpp
+++ src/test_vel_ctl.cpp
@@ -10,15 +10,16 @@ int main(int argc, char* argv[]) {
   while (ros::ok()) {
     float x,r;
     std::cin>>x>>r;
+    if (x == -1 && r == -1)break;
     geometry_msgs::Twist msg;
-    msg.linear.x = x/100 * MAX_LINEAR_SPEED;
+    msg.linear.x = x / 100 * MAX_LINEAR_SPEED;
     msg.linear.y = 0;
     msg.linear.z = 0;
     msg.angular.x = 0;
     msg.angular.y = 0;
-    msg.angular.z = r/100 * MAX_ANG_SPEED;
+    msg.angular.z = r / 100 * MAX_ANG_SPEED;
     publisher.publish(msg);
-    printf("send cmd_vel: X:%f, R:%f\n", x, r);
+    printf("send cmd_vel: X:%f, R:%f\n", msg.linear.x, msg.angular.z);
   }
   return 0;
 }
