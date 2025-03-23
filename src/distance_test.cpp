6c9d2c18a79605946e7416530a697d1bc89ac72d
dxd <2080776216@qq.com>
2025-03-23 21:20:18 +0800

    speed distance_test.cpp

--- src/distance_test.cpp
+++ src/distance_test.cpp
@@ -6,15 +6,26 @@ int main(int argc, char* argv[]) {
   ros::NodeHandle node_handle;
   const ros::Publisher twist_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 2);
   while (ros::ok()) {
-    double sleep_time;
+    double sleep_time, start_sleep_time;
+    ROS_INFO("Input start sleep");
+    std::cin>>start_sleep_time;
+    if (start_sleep_time == 0) {
+      ROS_INFO("DONE");
+      break;
+    }
+    ROS_INFO("Start Sleep for %lf", start_sleep_time);
     ROS_INFO("Input sleep");
     std::cin>>sleep_time;
     if (sleep_time == 0) {
       ROS_INFO("DONE");
       break;
     }
-    ROS_INFO("Sleep for %lf", sleep_time);
     geometry_msgs::Twist go_msg;
+    go_msg.linear.x = 0.2;
+    twist_pub.publish(go_msg);
+    ROS_INFO("START GO");
+    // ReSharper disable once CppExpressionWithoutSideEffects
+    ros::Duration(start_sleep_time).sleep();
     go_msg.linear.x = 0.7;
     twist_pub.publish(go_msg);
     ROS_INFO("GO");
