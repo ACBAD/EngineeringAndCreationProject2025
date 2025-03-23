c6494d08e8f5dc818872d42503fd9b2799fa7d5d
dxd <2080776216@qq.com>
2025-03-23 21:35:42 +0800

    speed distance_test.cpp

--- src/distance_test.cpp
+++ src/distance_test.cpp
@@ -6,31 +6,33 @@ int main(int argc, char* argv[]) {
   ros::NodeHandle node_handle;
   const ros::Publisher twist_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 2);
   while (ros::ok()) {
-    double sleep_time, start_sleep_time;
-    ROS_INFO("Input start sleep");
-    std::cin>>start_sleep_time;
-    if (start_sleep_time == 0) {
+    double end_vel, last_time;
+    ROS_INFO("Input start vel");
+    std::cin>>end_vel;
+    if (end_vel == 0) {
       ROS_INFO("DONE");
       break;
     }
-    ROS_INFO("Start Sleep for %lf", start_sleep_time);
-    ROS_INFO("Input sleep");
-    std::cin>>sleep_time;
-    if (sleep_time == 0) {
-      ROS_INFO("DONE");
-      break;
-    }
-    geometry_msgs::Twist go_msg;
-    go_msg.linear.x = 0.2;
-    twist_pub.publish(go_msg);
+    ROS_INFO("End vel is %lf", end_vel);
+    ROS_INFO("Input last time");
+    std::cin>>last_time;
     ROS_INFO("START GO");
-    // ReSharper disable once CppExpressionWithoutSideEffects
-    ros::Duration(start_sleep_time).sleep();
-    go_msg.linear.x = 0.7;
-    twist_pub.publish(go_msg);
-    ROS_INFO("GO");
-    // ReSharper disable once CppExpressionWithoutSideEffects
-    ros::Duration(sleep_time).sleep();
+    geometry_msgs::Twist go_msg;
+    for (double i = 0; i < end_vel ;i += 0.01) {
+      go_msg.linear.x = i;
+      twist_pub.publish(go_msg);
+      // ReSharper disable once CppExpressionWithoutSideEffects
+      ros::Duration(0.01).sleep();
+    }
+
+    if (last_time > 0) {
+      go_msg.linear.x = end_vel;
+      twist_pub.publish(go_msg);
+      ROS_INFO("GO");
+      // ReSharper disable once CppExpressionWithoutSideEffects
+      ros::Duration(last_time).sleep();
+    }
+
     go_msg.linear.x = 0;
     twist_pub.publish(go_msg);
     ROS_INFO("Stop");
