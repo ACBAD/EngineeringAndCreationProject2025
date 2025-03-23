7d52621d89e38b0d2a8592593340c4890949e502
dxd <2080776216@qq.com>
2025-03-23 21:46:52 +0800

    speed distance_test.cpp

--- src/distance_test.cpp
+++ src/distance_test.cpp
@@ -6,14 +6,17 @@ int main(int argc, char* argv[]) {
   ros::NodeHandle node_handle;
   const ros::Publisher twist_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 2);
   while (ros::ok()) {
-    double end_vel, last_time;
-    ROS_INFO("Input start vel");
+    double end_vel, last_time, time_step;
+    ROS_INFO("Input end vel");
     std::cin>>end_vel;
     if (end_vel == 0) {
       ROS_INFO("DONE");
       break;
     }
     ROS_INFO("End vel is %lf", end_vel);
+    ROS_INFO("Input time step");
+    std::cin>>time_step;
+    ROS_INFO("Time step is %lf", time_step);
     ROS_INFO("Input last time");
     std::cin>>last_time;
     ROS_INFO("START GO");
@@ -22,7 +25,7 @@ int main(int argc, char* argv[]) {
       go_msg.linear.x = i;
       twist_pub.publish(go_msg);
       // ReSharper disable once CppExpressionWithoutSideEffects
-      ros::Duration(0.01).sleep();
+      ros::Duration(time_step).sleep();
     }
 
     if (last_time > 0) {
