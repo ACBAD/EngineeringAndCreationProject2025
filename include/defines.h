8eaef03c6c9bc249efe0fba59e80886e4ed974d9
dxd <2080776216@qq.com>
2025-03-23 17:04:14 +0800

    fix defines.h

--- include/defines.h
+++ include/defines.h
@@ -5,7 +5,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 #include <geometry_msgs/Pose.h>
-constexpr double pi = 3.1415926535897932384626;
+constexpr double pi = 3.14159265358979323;
 
 #define DISTANCE_TOLERANCE_LIMIT 9999999999999
 #define ANGLE_TOLERANCE_LIMIT 199999999999999
@@ -29,36 +29,36 @@ public:
   UserSetPose(){}
   void init_red() {
     chosen_color = SIDE_RED;
-    poses[START_POSE].position.x = 0.27276426553726196;
-    poses[START_POSE].position.y = -0.0033133625984191895;
-    poses[START_POSE].orientation.z = -0.015136861496236815;
-    poses[START_POSE].orientation.w = 0.9998854311490111;
+    poses[START_POSE].position.x = 0.02624654769897461;
+    poses[START_POSE].position.y = -0.8079619407653809;
+    poses[START_POSE].orientation.z = 0.010020251335068935;
+    poses[START_POSE].orientation.w = 0.9999497960213712;
 
     poses[PICK_POSE].position.x = 3.0;
     poses[PICK_POSE].position.y = 1.5;
-    poses[PICK_POSE].position.z = 0.0;
+    poses[PICK_POSE].orientation.z = 0.0;
     poses[PICK_POSE].orientation.w = 1.0;
 
     poses[SECURITY_ZONE].position.x = 0.0;
     poses[SECURITY_ZONE].position.y = 0.0;
-    poses[SECURITY_ZONE].position.z = 0.0;
+    poses[SECURITY_ZONE].orientation.z = 0.0;
     poses[SECURITY_ZONE].orientation.w = 1.0;
   }
   void init_blue() {
     chosen_color = SIDE_BLUE;
-    poses[START_POSE].position.x = 1.0;
-    poses[START_POSE].position.y = 2.0;
-    poses[START_POSE].position.z = 0.5;
-    poses[START_POSE].orientation.w = 1.0;
+    poses[START_POSE].position.x = 0.02624654769897461;
+    poses[START_POSE].position.y = -0.8079619407653809;
+    poses[START_POSE].orientation.z = 0.010020251335068935;
+    poses[START_POSE].orientation.w = 0.9999497960213712;
 
     poses[PICK_POSE].position.x = 3.0;
     poses[PICK_POSE].position.y = 1.5;
-    poses[PICK_POSE].position.z = 0.0;
+    poses[PICK_POSE].orientation.z = 0.0;
     poses[PICK_POSE].orientation.w = 1.0;
 
     poses[SECURITY_ZONE].position.x = 0.0;
     poses[SECURITY_ZONE].position.y = 0.0;
-    poses[SECURITY_ZONE].position.z = 0.0;
+    poses[SECURITY_ZONE].orientation.z = 0.0;
     poses[SECURITY_ZONE].orientation.w = 1.0;
   }
 };
