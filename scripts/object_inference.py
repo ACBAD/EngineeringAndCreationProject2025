7c853af2d6f533f4b0289e2729903ebcddb7f4db
dxd <2080776216@qq.com>
2025-04-18 16:30:14 +0800

    debugging

--- scripts/object_inference.py
+++ scripts/object_inference.py
@@ -59,13 +59,13 @@ if __name__ == "__main__":
         objs_msg = ObjectInfoArray()
         objs_msg.data = cast(list, objs_msg.data)
         objs_msg.stamp = rospy.Time.now()
-        cast(np.ndarray, boxes)
         try:
             # Inference
             boxes, scores, classes = inference(cap_img, do_filter=True)
         except cv2.error as cv_err:
             print(f"{cv_err}")
             continue
+        cast(np.ndarray, boxes)
         if boxes is None:
             boxes = np.zeros(shape=(0, 4))
             classes = np.zeros(shape=(0, 4))
